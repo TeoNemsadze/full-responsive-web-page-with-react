1d6a1f742c7d39cc04c937752e72ff0cbae53f32
TeoNemsadze <87779348+TeoNemsadze@users.noreply.github.com>
2022-06-07 11:01:36 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Footer, Blog, Possibility, Feature, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Feature />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
+
+export default App;
